087028999e4d4c08457ef7af51730f2121831509
Banibu <guicjs14@gmail.com>
2022-12-05 13:59:39 -0300

    neko

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* =========================== typing animation ============================= */
+var typed = new Typed(".typing",{
+    strings:[",","Designer","Artista","Streamer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
